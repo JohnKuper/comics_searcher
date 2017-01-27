f32d915f89e7b9167d6fd68475312219d73f3d26
Dmitriy Korobeinikov <Dmitriy_Korobeinikov@epam.com>
2017-01-27 16:24:23 +0400

    Added circular reveal animation while opening the full poster.

--- app/src/main/java/com/korobeinikov/comicsviewer/ui/FullPosterActivity.java
+++ app/src/main/java/com/korobeinikov/comicsviewer/ui/FullPosterActivity.java
@@ -2,9 +2,12 @@ package com.korobeinikov.comicsviewer.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.korobeinikov.comicsviewer.R;
+import com.korobeinikov.comicsviewer.util.AnimationUtils;
 import com.squareup.picasso.Picasso;
 
 import butterknife.BindView;
@@ -15,6 +18,8 @@ public class FullPosterActivity extends AppCompatActivity {
     public static final String EXTRA_POSTER_URL = "EXTRA_POSTER_URL";
     public static final String THUMBNAIL_TRANSITION_NAME = "thumbnail";
 
+    @BindView(R.id.fullPosterRoot)
+    protected LinearLayout mRoot;
     @BindView(R.id.ivFullPoster)
     protected ImageView mFullPoster;
 
@@ -23,10 +28,21 @@ public class FullPosterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_full_poster);
         ButterKnife.bind(this);
-        showFullPoster();
+        startRevealAnimation();
+        loadFullPoster();
     }
 
-    private void showFullPoster() {
+    private void startRevealAnimation() {
+        mRoot.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+            @Override
+            public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+                v.removeOnLayoutChangeListener(this);
+                AnimationUtils.animateReveal(FullPosterActivity.this, mRoot, R.color.colorRipple);
+            }
+        });
+    }
+
+    private void loadFullPoster() {
         String fullPath = getIntent().getStringExtra(EXTRA_POSTER_URL);
         Picasso.with(this).load(fullPath).into(mFullPoster);
     }
--- app/src/main/java/com/korobeinikov/comicsviewer/util/AnimationUtils.java
+++ app/src/main/java/com/korobeinikov/comicsviewer/util/AnimationUtils.java
@@ -1,5 +1,12 @@
 package com.korobeinikov.comicsviewer.util;
 
+import android.animation.Animator;
+import android.content.Context;
+import android.support.annotation.ColorRes;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.Animation;
 
 /**
@@ -7,7 +14,26 @@ import android.view.animation.Animation;
  * Copyright (C) 2017 SportingBet. All rights reserved.
  */
 
-public class AnimationUtils {
+public final class AnimationUtils {
+
+    private AnimationUtils() {
+    }
+
+    public static void animateReveal(Context context, View view, @ColorRes int color) {
+        int centerX = (view.getLeft() + view.getRight()) / 2;
+        int centerY = (view.getTop() + view.getBottom()) / 2;
+        animateRevealColorFromCoordinates(context, view, color, centerX, centerY);
+    }
+
+    private static void animateRevealColorFromCoordinates(Context context, View view, @ColorRes int color, int centerX, int centerY) {
+        float finalRadius = (float) Math.hypot(view.getWidth(), view.getHeight());
+
+        Animator anim = ViewAnimationUtils.createCircularReveal(view, centerX, centerY, 0, finalRadius);
+        view.setBackgroundColor(ContextCompat.getColor(context, color));
+        anim.setDuration(750);
+        anim.setInterpolator(new AccelerateDecelerateInterpolator());
+        anim.start();
+    }
 
     public static class AnimationListenerAdapter implements Animation.AnimationListener {
         @Override

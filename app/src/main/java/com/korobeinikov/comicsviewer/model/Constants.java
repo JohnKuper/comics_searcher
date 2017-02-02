0f660dbb09293a49ef4f8d4b2164259227f2b876
Dmitriy Korobeinikov <Dmitriy_Korobeinikov@epam.com>
2017-02-02 17:25:39 +0400

    Added comment about API keys.

--- app/src/main/java/com/korobeinikov/comicsviewer/model/Constants.java
+++ app/src/main/java/com/korobeinikov/comicsviewer/model/Constants.java
@@ -5,6 +5,7 @@ package com.korobeinikov.comicsviewer.model;
  */
 public class Constants {
 
+    //This is not the best practice to save these variables in the public domain, but for the sample app, it's enough
     public static final String PUBLIC_MARVEL_KEY = "46876daf61600cab0a15f2481457a52e";
     public static final String PRIVATE_MARVEL_KEY = "e76f8ff957fefbe1f62ae2b2f481d3b059e5046d";
 }
